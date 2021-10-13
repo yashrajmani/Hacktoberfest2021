43fe4216ae32261f8062290c28cef41a28088a4e
ceccs18c62 <60084945+ceccs18c62@users.noreply.github.com>
2021-10-13 07:53:26 +0530

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -24,6 +24,7 @@ rand_upper = random.choice(UPCASE_CHARACTERS)
 rand_lower = random.choice(LOCASE_CHARACTERS)
 rand_symbol = random.choice(SYMBOLS)
 
+#ensuring password has digit,lower and upper character and symbols
 temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
   
 for x in range(MAX_LEN - 4):
