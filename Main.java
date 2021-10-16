83f4df5e845846aa709c5b0b838ca87785e44ac3
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2021-10-17 00:42:50 +0530

    string related program
    There are two programs:
    
    1. Counting string length ,by converting integer to string
    2. Displaying byte, short, integer, long , and double value of a number

---
+++ Main.java
@@ -0,0 +1,21 @@
+//program 1
+public class Main {
+public static void main(String[] args) {
+Integer myInt = 7000;
+String myString = myInt.toString();
+System.out.println(myString.length());
+}
+}
+//program 2
+public class Main { 
+    public static void main(String[] args) 
+    { 
+        Integer I =Integer.valueOf(130); 
+        System.out.println(I.byteValue()); 
+        System.out.println(I.shortValue()); 
+        System.out.println(I.intValue()); 
+        System.out.println(I.longValue()); 
+        System.out.println(I.floatValue()); 
+        System.out.println(I.doubleValue()); 
+    } 
+} 
