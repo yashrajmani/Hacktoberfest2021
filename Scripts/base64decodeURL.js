5855c854be36bd004cfac1a3b2936a7b6b88968a
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-10-20 07:31:04 +0700

    Create a Base64 URL decoder

--- Scripts/base64decodeURL.js
+++ Scripts/base64decodeURL.js
@@ -1,1 +1,6 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Awal Ariansyah
+// GITHUB: https://github.com/snowfluke
+
 const decode = base64 => base64.includes('=aHR') ? atob(base64) : "not a valid base64 url";
