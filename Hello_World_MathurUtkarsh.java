faeefe00140dd7e5c631b9eb1f0ad36e836ef7a0
Utkarsh Mathur <78642104+MathurUtkarsh@users.noreply.github.com>
2021-10-21 22:34:36 +0530

    Create Hello_World_MathurUtkarsh.java
    Created a Hello World program using java.

---
+++ Hello_World_MathurUtkarsh.java
@@ -0,0 +1,12 @@
+// Language : Java
+// Author : Utkarsh Mathur
+// Github : https://github.com/MathurUtkarsh
+
+// Hello World Program
+
+
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
