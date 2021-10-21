61485aca0c5616ae37ff4660c83d89794c96325a
Utkarsh Mathur <78642104+MathurUtkarsh@users.noreply.github.com>
2021-10-21 22:28:26 +0530

    Create Hello_World_MathurUtkarsh..cs
    Created Hello World Program Using C#.

---
+++ Scripts/Hello_World_MathurUtkarsh..cs
@@ -0,0 +1,13 @@
+// Language : C#
+// Author : Utkarsh Mathur
+// Github : https://github.com/MathurUtkarsh
+
+namespace HelloWorld
+{
+    class Hello {        
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
