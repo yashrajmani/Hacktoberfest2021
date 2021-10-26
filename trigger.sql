6fba0a6ee6bbb6dc46dc0b2f7e978c54d2ccafad
Anaa29 <92617694+Anaa29@users.noreply.github.com>
2021-10-26 21:59:07 +0530

    Update trigger.sql
    commenting irrelevant statements

--- trigger.sql
+++ trigger.sql
@@ -1,6 +1,6 @@
 -- this query will basically tells us about any new info added about which need information if else can be used for required conditions.
-   imp note "From delimiter $$ run in command line client as it is a trigger till delimiter ;",after wriitng the above query query of insertion of new row will update trigger table
-   and add the msg u can also add column name of table on which trigger is applied.
+-- imp note "From delimiter $$ run in command line client as it is a trigger till delimiter ;",after wriitng the above query query of insertion of new row will update trigger table
+--  and add the msg u can also add column name of table on which trigger is applied.
    
  -- for workbench
  create table trigger_test(
